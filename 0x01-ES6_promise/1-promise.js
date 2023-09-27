0852738ef75d4fbcf7d508c4c85dd47763da3d63
Mamosa Monatisa <paballomonatisa7@gmail.com>
2023-09-27 08:39:57 +0000

    Don't make a promise...if you know you can't keep it

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,17 @@
+const getFullResponseFromAPI = (success) => {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      const response = {
+        status: 200,
+        body: 'Success'
+      };
+      resolve(response);
+    } else {
+      const error = new Error('The fake API is not working currently');
+      reject(error);
+    }
+  });
+};
+
+export default getFullResponseFromAPI;
+
