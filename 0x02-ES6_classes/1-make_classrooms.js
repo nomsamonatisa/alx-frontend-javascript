a71fcf1536c311d29f34716ca662237479e37cc7
Mamosa Monatisa <paballomonatisa7@gmail.com>
2023-09-28 17:09:17 +0000

    new code

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,17 +1,9 @@
-// Import the ClassRoom class from 0-classroom.js
-const ClassRoom = require('./0-classroom');
+import ClassRoom from './0-classroom';
 
-// Define the initializeRooms function
-function initializeRooms() {
-  // Create an array of ClassRoom objects with the specified sizes
-  const rooms = [
+export default function initializeRooms() {
+  return [
     new ClassRoom(19),
     new ClassRoom(20),
-    new ClassRoom(34)
+    new ClassRoom(34),
   ];
-  return rooms;
 }
-
-// Export the initializeRooms function
-module.exports = initializeRooms;
-
